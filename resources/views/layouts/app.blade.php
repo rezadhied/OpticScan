a58a1c60b2130df584ef419a61830a698e2220e0
Marsyarayyy <marsyarh09@gmail.com>
2024-04-02 18:32:59 +0700

    dashboardDokter
    membuat tampilan dashboard dokter (1)

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="icon" href="/img/logo.png" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- font,bootstrap,css -->
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,600" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/dashboard.css">
+    <!-- Scripts -->
+    
+</head>
+<body>
+    <div id="app" class = "d-flex">
+        <div class="d-flex flex-column flex-shrink-0 p-3 " style="width: 280px; background-color: rgba(19, 39, 104, 1);">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+                <span class="fs-4"><img src="/img/logo.png" alt=""></span>
+            </a>
+            <hr>
+            <ul class="nav nav-pills flex-column mb-auto">
+                <li class="nav-item">
+                <a href="#" class="nav-link d-flex align-items-center gap-2 text-white " aria-current="page">
+                    <img src="/img/home.png" alt="">
+                    Beranda
+                </a>
+                </li>
+                <li>
+                <a href="#" class="nav-link d-flex align-items-center gap-2 text-white " aria-current="page">
+                <img src="/img/people.png" alt="">
+                    Pasien
+                </a>
+                </li>
+
+            </ul>
+            <hr>
+            <div class="dropdown">
+                <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                <strong>mdo</strong>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+                <li><a class="dropdown-item" href="#">Settings</a></li>
+                <li><a class="dropdown-item" href="#">Profile</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Log out</a></li>
+                </ul>
+            </div>
+        </div class = "w-full">
+            <div><header class="site-header">
+                <div class="container">
+                    <div class="site-header-inner">
+                        <div class="brand header-brand">
+                            <h1 class="fs-4">
+                                Halo, Dr Andri
+                            </h1>
+                            <hr>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 
 
-Route::get('/', [RegisterController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index']);
