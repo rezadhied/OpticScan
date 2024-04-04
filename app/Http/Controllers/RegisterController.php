40df70ac665989fc36b0a0f086e4e7568a2b2876
rezadhied <rezadhied@student.telkomuniversity.ac.id>
2024-04-04 23:00:56 +0700

    account register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -11,4 +11,27 @@ class RegisterController extends Controller
     public function index(){
         return view ('register');
     }   
+
+    public function store(Request $request){
+        
+        $request->validate([
+            'name' => 'required|min:4',
+            'username' => 'required|min:4|unique:model_user',
+            'email' => 'required|email|unique:model_user',
+            'password' => 'required',
+            'confirmation' => 'required|same:password',
+        ], [
+            'username.unique' => 'Username sudah terdaftar.',
+            'email.unique' => 'Email sudah terdaftar.',
+        ]);
+
+        $data =  new ModelUser();
+        $data->name = $request->name;
+        $data->username = $request->username;
+        $data->email = $request->email;
+        $data->password = Hash::make($request->password);
+        $data->role = 'pasien'; // Set default role sebagai pasien
+        $data->save();
+        return redirect('/')->with('alert-success','Register Success');
+    }
 }
