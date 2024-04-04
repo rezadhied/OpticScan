03039ffc9ce93c297f5fd69658c2ffb0a8584522
rezadhied <rezadhied@student.telkomuniversity.ac.id>
2024-04-04 23:14:29 +0700

    login backend

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 
 Route::get('/', [DashboardController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index']);
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
