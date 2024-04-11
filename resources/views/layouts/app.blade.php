5314bdc03861c0cd012277010eea00083ac3c79a
RaihanSulthon <raihansy@student.telkomuniversity.ac.id>
2024-04-11 12:38:40 +0700

    updatte

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -13,63 +13,59 @@
     <!-- font,bootstrap,css -->
     <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,600" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/dashboard.css">
-    <!-- Scripts -->
-    
 </head>
 <body>
-    <div id="app" class = "d-flex">
-        <div class="d-flex flex-column flex-shrink-0 p-3 " style="width: 280px; background-color: rgba(19, 39, 104, 1);">
-            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+    <div id="app" class="d-flex">
+        <div class="d-flex flex-column p-3 w-auto vh-100" style="background-color: rgba(19, 39, 104, 1); flex-basis: 280px;">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none pe-5">
                 <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-                <span class="fs-4"><img src="/img/logo.png" alt=""></span>
+                <span class="fs-6"><img src="/img/logo.png" alt=""></span>
             </a>
             <hr>
             <ul class="nav nav-pills flex-column mb-auto">
                 <li class="nav-item">
-                <a href="#" class="nav-link d-flex align-items-center gap-2 text-white " aria-current="page">
-                    <img src="/img/home.png" alt="">
-                    Beranda
-                </a>
+                    <a href="#" class="nav-link d-flex align-items-center gap-2 text-white " aria-current="page">
+                        <img src="/img/home.png" alt="">
+                        Beranda
+                    </a>
                 </li>
+                <br>
                 <li>
-                <a href="#" class="nav-link d-flex align-items-center gap-2 text-white " aria-current="page">
-                <img src="/img/people.png" alt="">
-                    Pasien
-                </a>
+                    <a href="#" class="nav-link d-flex align-items-center gap-2 text-white " aria-current="page">
+                        <img src="/img/people.png" alt="">
+                        Pasien
+                    </a>
                 </li>
-
             </ul>
             <hr>
             <div class="dropdown">
                 <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src= "/img/profile.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                <!-- <strong>mdo</strong> -->
+                    <img src="/img/profile.png" alt="" width="32" height="32" class="rounded-circle me-2">
                 </a>
                 <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                <li><a class="dropdown-item" href="#">Settings</a></li>
-                <li><a class="dropdown-item" href="#">Profile</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Log out</a></li>
+                    <li><a class="dropdown-item" href="#">Settings</a></li>
+                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Log out</a></li>
                 </ul>
             </div>
         </div>
-            <div><header class="site-header">
+        <!-- Main content -->
+        <div class="flex-grow-1">
+            <header class="site-header">
                 <div class="container">
                     <div class="site-header-inner">
                         <div class="brand header-brand ">
                             <div class="title">
-                            <h1 class=" fs-4" >
-                                Halo, Dr Andri! 
-                            </h1>
+                                <h1 class="fs-4">Halo, Dr Andri!</h1>
                             </div>
-                            <p style="font-size: small;color:grey" >Selamat Datang, Selamat Beraktivitas</p>
+                            <p style="font-size: small;color:grey">Selamat Datang, Selamat Beraktivitas</p>
                             <hr>
                         </div>
                     </div>
                 </div>
             </header>
-            <main class="py-4 w-full">
+            <main class="py-4">
                 @yield('content')
             </main>
         </div>
