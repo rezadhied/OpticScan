b953aa8df7928ff949400eafb85f62a92c3d03ea
RaihanSulthon <raihansy@student.telkomuniversity.ac.id>
2024-05-03 04:36:06 +0700

    Input Data Pasien Done

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\InputDataPasien;
 
 
 Route::get('/', [DashboardController::class, 'index']);
 Route::get('/login', [LoginController::class, 'index']);
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+Route::get('/DataInput', [InputDataPasien::class, 'index']);
